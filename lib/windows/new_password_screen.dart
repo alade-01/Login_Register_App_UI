e67f6eb7a398edc7d26fcdb30e7222b1985e1a26
alade-01 <aladewilliame1998@gmail.com>
2023-11-30 22:09:44 -0500

    clear new password screen

--- lib/windows/new_password_screen.dart
+++ lib/windows/new_password_screen.dart
@@ -31,144 +31,127 @@ class _NewPasswordScreenState extends State<NewPasswordScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-        child: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height: BUTTON_SEPARATION_SPACE * 3),
-              Text(
-                "Create new password",
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: secondaryColor,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,),
-              ),
-              const SizedBox(height: BUTTON_SEPARATION_SPACE * 2),
-              Text(
-                "Your new password must be unique from those previously used.",
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: Color(0xFF8391A1),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: BUTTON_SEPARATION_SPACE * 2),
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 17),
-                      child: TextFormField(
-                        validator: (String? value) {
-                          if (value != null && value.isEmpty) {
-                            return "This field is required";
-                          }
-                          return null;
-                        },
-                        style: textStyleInput,
-                        controller: _passwordController,
-                        obscureText: _obscureTextFieldOne,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(
-                            CupertinoIcons.lock,
-                          ),
-                          hintText: "New Password",
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _obscureTextFieldOne =
-                                !_obscureTextFieldOne;
-                              });
-                            },
-                            child: Icon(
-                              _obscureTextFieldOne
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                            ),
-                          ),
-                        ),
-                        keyboardType: TextInputType.text,
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: BUTTON_SEPARATION_SPACE * 3),
+                      Text(
+                        "Create new password",
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: secondaryColor,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,),
                       ),
-                    ),
-                    Container(
-                      child: TextFormField(
-                        validator: (String? value) {
-                          if (value != null && value.isEmpty) {
-                            return "This field is required";
-                          }
-                          if (_passwordControllerConfirm !=
-                              _passwordController) {
-                            return "Different password";
-                          }
-                          return null;
-                        },
-                        style: textStyleInput,
-                        controller: _passwordControllerConfirm,
-                        obscureText: _obscureTextFieldTwo,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(
-                            CupertinoIcons.lock,
-                          ),
-                          hintText: "Confirm Password",
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _obscureTextFieldTwo =
-                                !_obscureTextFieldTwo;
-                              });
-                            },
-                            child: Icon(
-                              _obscureTextFieldTwo
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                            ),
-                          ),
-                        ),
-                        keyboardType: TextInputType.text,
+                      const SizedBox(height: BUTTON_SEPARATION_SPACE * 2),
+                      Text(
+                        "Your new password must be unique from those previously used.",
+                        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            color: Color(0xFF8391A1),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: BUTTON_SEPARATION_SPACE * 2),
+                      Form(
+                        key: formKey,
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 17),
+                              child: TextFormField(
+                                validator: (String? value) {
+                                  if (value != null && value.isEmpty) {
+                                    return "This field is required";
+                                  }
+                                  return null;
+                                },
+                                style: textStyleInput,
+                                controller: _passwordController,
+                                obscureText: _obscureTextFieldOne,
+                                decoration: InputDecoration(
+                                  prefixIcon: const Icon(
+                                    CupertinoIcons.lock,
+                                  ),
+                                  hintText: "New Password",
+                                  suffixIcon: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _obscureTextFieldOne =
+                                        !_obscureTextFieldOne;
+                                      });
+                                    },
+                                    child: Icon(
+                                      _obscureTextFieldOne
+                                          ? Icons.visibility_off
+                                          : Icons.visibility,
+                                    ),
+                                  ),
+                                ),
+                                keyboardType: TextInputType.text,
+                              ),
+                            ),
+                            Container(
+                              child: TextFormField(
+                                validator: (String? value) {
+                                  if (value != null && value.isEmpty) {
+                                    return "This field is required";
+                                  }
+                                  if (_passwordControllerConfirm !=
+                                      _passwordController) {
+                                    return "Different password";
+                                  }
+                                  return null;
+                                },
+                                style: textStyleInput,
+                                controller: _passwordControllerConfirm,
+                                obscureText: _obscureTextFieldTwo,
+                                decoration: InputDecoration(
+                                  prefixIcon: const Icon(
+                                    CupertinoIcons.lock,
+                                  ),
+                                  hintText: "Confirm Password",
+                                  suffixIcon: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _obscureTextFieldTwo =
+                                        !_obscureTextFieldTwo;
+                                      });
+                                    },
+                                    child: Icon(
+                                      _obscureTextFieldTwo
+                                          ? Icons.visibility_off
+                                          : Icons.visibility,
+                                    ),
+                                  ),
+                                ),
+                                keyboardType: TextInputType.text,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: BUTTON_SEPARATION_SPACE * 4),
+                      AppButton(
+                        callback: () {
+                          Navigator.pushNamed(context, RouterGenerator.passwordChangedRoute);
+                          // if (formKey.currentState!.validate()) {
+                          //   Navigator.pushNamed(context, RouterGenerator.passwordChangedRoute);
+                          // }
+                        },
+                        label: "Reset Password",
+                        buttonType: ButtonType.PRIMARY,
+                        width: size.width,
+                      ),
+                      const SizedBox(height: BUTTON_SEPARATION_SPACE * 4),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: BUTTON_SEPARATION_SPACE * 4),
-              AppButton(
-                callback: () {
-                  Navigator.pushNamed(context, RouterGenerator.passwordChangedRoute);
-                  // if (formKey.currentState!.validate()) {
-                  //   Navigator.pushNamed(context, RouterGenerator.passwordChangedRoute);
-                  // }
-                },
-                label: "Reset Password",
-                buttonType: ButtonType.PRIMARY,
-                width: size.width,
-              ),
-              const SizedBox(height: BUTTON_SEPARATION_SPACE * 4),
-              RichText(
-                text: TextSpan(
-                  text: 'Remember Password?',
-                  style:  Theme.of(context)
-                      .textTheme
-                      .labelMedium!
-                      .copyWith(
-                      fontSize: 15,
-                      color: Color(0xFF24282C),
-                      fontWeight: FontWeight.w500),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: 'Login',
-                        style:  Theme.of(context)
-                            .textTheme
-                            .labelMedium!
-                            .copyWith(
-                            fontSize: 15,
-                            color: primaryColor,
-                            fontWeight: FontWeight.w700)
-                    )
-                  ],
-                ),
-              )
             ],
           ),
         ));
